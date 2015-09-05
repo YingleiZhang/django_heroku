97ec2d9a197697dc59b4842774bbce1c285ba3f3
Yinglei Zhang <yinglei.zh@gmail.com>
2015-09-05 16:26:02 -0400

    fix the url

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.BlogIndex.as_view(), name="index"),
-	url(r'^$', views.userlogin, name="login"),
+	url(r'^userlogin', views.userlogin, name="login"),
 	url(r'^feed/$', views.BlogFeed.as_view(), name="feed"),
 	url(r'^post/(?P<slug>\S+)$', views.BlogDetail.as_view(), name="post_detail"),
 ]
