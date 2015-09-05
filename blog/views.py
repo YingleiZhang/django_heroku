d1f765ca4d8a539b8541af95eb164eeebe0949e2
Yinglei Zhang <yinglei.zh@gmail.com>
2015-09-05 16:24:03 -0400

    try facebook login on heroku

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 from django.views import generic
 from django.contrib.auth.models import User
 from . import models
 
 # Create your views here.
+def userlogin(request):
+	context = RequestContext(request, {'request':request, 'user':request.user})
+	return render_to_response('blog/login.html',context_instance=context)
+"""
 class BlogLogin(generic.ListView):
 	model = User
 	template_name = "blog/login.html"
-
+"""
 class BlogFeed(generic.ListView):
 	queryset = models.Post.objects.published()[:5]
 	template_name = "blog/feed.html"
