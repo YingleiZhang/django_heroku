9b03aebc195b1ed457217197d836bd928aeaeffa
Yinglei Zhang <yinglei.zh@gmail.com>
2015-09-06 13:32:16 -0400

    add django-disqus feature for comments

--- herokuapp/settings.py
+++ herokuapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.site',
+    'disqus',
     'django_markdown',
     'bootstrap3',
     'social.apps.django_app.default',
