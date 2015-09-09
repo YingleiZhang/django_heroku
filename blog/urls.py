7562aa0e049f80e6f4e3acd9077e0dc81c574c7a
Yinglei Zhang <yinglei.zh@gmail.com>
2015-09-08 20:28:24 -0400

    fix a url bug with disqus

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.BlogIndex.as_view(), name="index"),
-	url(r'^userauth', views.userauth, name="userauth"),
+	url(r'^userauth/', views.userauth, name="userauth"),
 	url(r'^feed/$', views.BlogFeed.as_view(), name="feed"),
 	url(r'^post/(?P<slug>\S+)$', views.BlogDetail.as_view(), name="post_detail"),
 ]
